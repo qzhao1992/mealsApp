132b6b588f4f7c98e19ed65785340caa51a01194
Qing <qzhao@uco.edu>
2022-02-26 17:59:28 -0600

    working on category

---
+++ lib/category_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CategotyItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategotyItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import './models/category.dart' as category;
+
+// import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  category.Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  category.Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  category.Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  category.Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  category.Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  category.Category(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  category.Category(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  category.Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  category.Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  category.Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
